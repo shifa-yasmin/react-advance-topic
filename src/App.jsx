6710fe627975d8c005659762785eccc0ad94db34
shifa <shifayasmin23@gmail.com>
2026-07-11 16:17:40 +0530

    useReducer-toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import React, { useReducer } from 'react'
-  const reducer=(state,action)=>{
-    if(action.type==="inc"){
-      return state+1
-    }
-    if(action.type==="dec"){
-      return state-1
-    }
+import  { useReducer } from 'react'
+
+const reducer=(state,action)=>{
+  if(action.type==="toggle"){
+    return state==="red"?"pink":"red"
   }
+}
 const App = () => {
-  const [state,dispatch]=useReducer(reducer,0)
+    const [state,dispatch]=useReducer(reducer,"red")
   return (
-    <div>
-      <h1>count is:{state}</h1>
-      <button onClick={()=>dispatch({type:"inc"})}>+</button>
-      <button onClick={()=>dispatch({type:"dec"})}>-</button>
+    <div style={{background:state, height:"100vh"}}>
+      <button onClick={()=>dispatch({type:"toggle"})}>click</button>
     </div>
   )
 }
