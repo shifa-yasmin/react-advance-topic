2c15b6e995f257ed0131fe5751f0dafbe6de5bc2
shifa <shifayasmin23@gmail.com>
2026-07-11 19:18:40 +0530

    navigation

--- src/About.jsx
+++ src/About.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
-
+import  { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from "axios"
 const About = () => {
+    const {id}=useParams();
+    const [data,setData]=useState({});
+    useEffect(()=>{
+     axios.get(`http://localhost:3000/students/${id}`)
+     .then((res)=>{
+        setData(res.data);
+        console.log(res.data)
+     })
+    },[])
   return (
-    <div>About</div>
+    <div>
+       <h1>{data.name}</h1>   
+       <h1>{data.age}</h1> 
+       <h1>{data.course}</h1>
+    </div>
   )
 }
 
