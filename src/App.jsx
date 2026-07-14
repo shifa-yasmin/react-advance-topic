630eba27acf4630c52a3aaac73330443b3fc3c5d
shifa <shifayasmin23@gmail.com>
2026-07-14 13:48:29 +0530

    pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,47 @@
-import {  useState } from 'react'
+import  { useEffect, useState } from 'react'
 import axios from "axios"
 const App = () => {
-  const [data,setData]=useState([]);
-  const [name,setName]=useState("");
-  const [email,setEmail]=useState("")
- const handle=()=>{
-  axios.post("http://localhost:3000/students",{
-    name,email
-  }).then((res)=>{
-    setData(res.data)
-    console.log(res.data)
-  })
- }
-  return (
-    <div>
-      <h1>form </h1>
-      <input value={name} onChange={(e)=>setName(e.target.value)} placeholder='enter name'/>
-      <br/>
-       <input value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='enter email'/>
-      <br/>
-      <button onClick={handle}>submit</button>
 
+  const [currentpage,setCurrentpage]=useState(1);
+  const productperpage=3;
+  const [data,setData]=useState([]);
+  useEffect(()=>{
+      axios.get("http://localhost:3000/students")
+      .then((res)=>{
+        console.log(res.data);
+        setData(res.data)
+      })
+  },[])
+  const lastIndex=currentpage*productperpage;
+  const firstIndex=lastIndex-productperpage;
+  const currentproducts=data.slice(firstIndex,lastIndex);
+  const totelpage=Math.ceil(data.length/productperpage)
+  return (
+    <div className='flex items-center justify-center '>
+      <table className='border border-black'>
+        <thead >
+           <tr>
+          <th className=' border border-black'>name</th>
+          <th className=' border border-black'>email</th>
+         </tr>
+        </thead>
+        <tbody>
+          {currentproducts.map((n)=>(
+            <tr >
+              <td className=' border border-black'>{n.name}</td>
+              <td className=' border border-black'>{n.department}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+        
+         <div>
+         <button disabled={currentpage===1} onClick={()=>setCurrentpage(currentpage-1)} >prev</button>
+         <span>page {currentpage} to {totelpage}</span>
+         <button disabled={currentpage===totelpage} onClick={()=>setCurrentpage(currentpage+1)}>next</button>
+         </div>
     </div>
+   
   )
 }
 
