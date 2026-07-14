2c781e50f79170ba1e1d1a99a299eaffb7e6bd3a
shifa <shifayasmin23@gmail.com>
2026-07-14 13:01:39 +0530

    fetch post

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import  { useEffect, useState } from 'react'
-// import axios from "axios"
+import {  useState } from 'react'
+
 const App = () => {
   const [data,setData]=useState([]);
-  useEffect(()=>{
-     fetch("http://localhost:3000/students")
-     .then((res1)=>res1.json())
-     .then((res)=>{
-      setData(res);
-      console.log(res)
-     })
-  },[])
+  const [name,setName]=useState("");
+  const [email,setEmail]=useState("")
+  const handle=()=>{
+    fetch("http://localhost:3000/students",{
+        method:"post",
+        headers:{
+          "Content-type":"Application/json",
+        },
+        body:JSON.stringify({
+          name,
+          email
+        })
+       })
+       .then((res)=>res.json())
+        .then((data)=>{
+          setData(data)
+          console.log(data)
+        })
+  }
   return (
     <div>
-        {data.map((n)=>(
-          <p>{n.name}</p>
-        ))}
+      <h1>form </h1>
+      <input value={name} onChange={(e)=>setName(e.target.value)} placeholder='enter name'/>
+      <br/>
+       <input value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='enter email'/>
+      <br/>
+      <button onClick={handle}>submit</button>
+
     </div>
   )
 }
