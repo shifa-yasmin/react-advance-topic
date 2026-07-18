feeff2adfe5467d0191bc43c16d345ab411f72ae
shifa <shifayasmin23@gmail.com>
2026-07-18 08:20:04 +0530

    useRef-input

--- src/UseRef.jsx
+++ src/UseRef.jsx
@@ -1,15 +1,14 @@
-import  { useRef } from 'react'
+import React, { useRef } from 'react'
 
 const UseRef = () => {
-    const ref=useRef(0);
-    const handle=()=>{
-       ref.current++
-       console.log(ref.current)
-    }
+    const ref=useRef();
+   const handle=()=>{
+    ref.current.focus();
+   }
   return (
     <div>
-  <h1>count is:{ref.current}</h1>
-  <button onClick={handle}>+</button>
+   <input ref={ref} />
+   <button onClick={handle}>focus</button>
     </div>
   )
 }
